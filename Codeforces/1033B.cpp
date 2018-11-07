23d78ae64e7e5fde5313f9d9d3ee62895780dbd9
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-11-07 12:30:10 -0600

    more Codeforces solutions

---
+++ Codeforces/1033B.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+bool isPrime(ull a){
+    FOR(i,2,sqrt(a)+1,1){
+        if(a%i==0)return false;
+    }
+    return true;
+}
+
+int main(){
+sync;
+    ull a, b; 
+    int n; 
+    cin>>n; 
+    while(n--){
+    cin>>a>>b; 
+    if(a-b==1&&isPrime(a+b)){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    }
+}
---
+++ Codeforces/96A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+string s;
+cin>>s; 
+int cuenta = 0; 
+for(int i=0; i<s.length()-1; i++){
+    if(s[i]==s[i+1])cuenta++;
+    if(s[i]!=s[i+1])cuenta = 0;
+    if(cuenta>=6){cout<<"YES"<<endl; return 0;}
+}
+cout<<"NO"<<endl;
+
+return 0;
+}
+//110000000
