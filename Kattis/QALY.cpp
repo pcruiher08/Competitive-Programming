4e7e7218cd14652280fd521d6550168f8af76590
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-11-09 10:57:31 -0600

    added more solutions

---
+++ Kattis/QALY.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+
+int main(){
+sync;
+int n; 
+cin>>n; 
+double a, b;
+double sum = 0; 
+    while(n--){
+        cin>>a>>b; 
+        sum+=a*b;
+    }
+cout<<sum;
+return 0;
+}
+
+/*
+5
+1.0 12.0
+0.7 5.2
+0.9 10.7
+0.5 20.4
+0.2 30.0
+*/
