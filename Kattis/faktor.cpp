c6e2981e8e7df13a2d722de4ca2e2ae52ce2825c
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-11-11 11:58:33 -0600

    more Kattiss solutions

---
+++ Kattis/faktor.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+int a, b; 
+cin>>a>>b; 
+
+int i = 0; 
+while((double)ceil((double)i*1.0/a)!=b){
+    i++;
+}
+cout<<i;
+
+return 0;
+}
