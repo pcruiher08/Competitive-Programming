0294741f5d7b052691a3f2bd767245487fd992d2
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-11-12 19:48:16 -0600

    more soluttions

---
+++ OmegaUp/triples.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+void imprime(int arr[], int n){
+    int cuenta = 0;
+    vector <int> vec; 
+    FOR(i,0,n,1){
+        if(arr[i]%3==0){
+            cuenta++;
+            vec.pb(i+1);
+        }
+    }
+    if(cuenta == 0){
+        cout<<"No hay triples.";
+    }else{
+        cout<<vec.size()<<endl;
+        FOR(i,0,vec.size(),1)if(i<vec.size()-1)cout<<vec[i]<<" ";else cout<<vec[i];
+    }
+}
+
+int main(){
+sync;
+int n;
+cin>>n; 
+
+int arr[n];
+
+FOR(i,0,n,1)cin>>arr[i];
+imprime(arr, n);
+}
