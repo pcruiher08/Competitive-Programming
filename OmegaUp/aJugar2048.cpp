df972c782f8ce55d55d0c4ca7adde1690d8a0eda
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-12-16 14:50:52 -0600

    new omegaup solutions

--- OmegaUp/aJugar2048.cpp
+++ OmegaUp/aJugar2048.cpp
@@ -15,26 +15,40 @@
 #include <climits>
 #define ll long long
 #define ull unsigned long long
-#define FOR(m,s,n,u) for(ull m=s; m<n; m+=u)
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
 #define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
 #define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
 #define pb push_back
 #define mod 1000000007
 #define MOD(x) ((x%mod)+mod)%mod
 #define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define vi vector<ull>
+#define vi vector<int>
 using namespace std;
 
 int main(){
 sync;
-ull n;
+
+int n;
 cin>>n;
-ull arr[n];
-FOR(i,0,n,1)cin>>arr[i];
-vi edificios;
-ull cuenta = 1;
-FOR(i,0,n,1){if(arr[i]>arr[i+1]){cuenta++;}else{edificios.pb(cuenta);cuenta = 1;}}
-sort(edificios.begin(), edificios.end());
-cout<<edificios[edificios.size()-1]<<endl;
+stack <int> s;
+int x;
+int aux = 0;
+while(n--){
+    cin>>x;
+    aux = x;
+    while(!s.empty()&&s.top()==aux){
+        aux += aux;
+        s.pop();
+    }
+    s.push(aux);
+    if(s.empty())s.push(x);
+}
+
+cout<<s.size()<<endl;
+while(!s.empty()){
+    cout<<s.top()<<endl;
+    s.pop();
+}
+
 return 0;
 }
