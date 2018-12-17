d8f2d48d5d2c0ab7555f00318315f69f32d8a903
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-12-16 20:32:23 -0600

    new omegaup solutions

---
+++ OmegaUp/laPlayeraDePablo.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#include <queue>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+ull n; 
+cin>>n;
+ull x;
+priority_queue <ull> pq;
+
+FOR(i,0,n,1)cin>>x, pq.push(x);
+
+pq.pop();
+pq.pop();
+cout<<pq.top();
+return 0;
+
+/*
+5
+5 2 4 5 6
+*/
+}
