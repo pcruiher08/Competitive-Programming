e2c09dc693ae3dcc2045f7d34bd649f899fc7c29
Pablo C. Ruíz <pcruiher089@gmail.com>
2018-12-20 00:05:01 -0600

    omegaUp solutions

---
+++ OmegaUp/esMayorOMenorDeEdad.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+
+    if(a>=18){
+        cout<<"Es Mayor"<<endl;
+    }else{
+        cout<<"Es Menor"<<endl;
+    }
+}
