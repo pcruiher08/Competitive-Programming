753904c487290784c333a03965a8e032041f8b38
unknown <pcruiher089@gmail.com>
2019-01-01 18:46:56 -0600

    codeforces

---
+++ Codeforces/116A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+int n; cin>>n;
+int adentro = 0;
+int max = 0;
+int x, y;
+while(n--){
+    cin>>x>>y;
+    adentro-=x;
+    adentro+=y;
+    if(adentro>max)max = adentro;
+}
+cout<<max<<endl;
+return 0;
+}
---
+++ Codeforces/263A.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+int mat[5][5];
+int iLoc, jLoc;
+FOR(i,0,5,1)FOR(j,0,5,1){cin>>mat[i][j];if(mat[i][j]==1){iLoc = i; jLoc = j;}}
+
+int res = 0; 
+
+
+res += (abs(3-iLoc-1)+abs(3-jLoc-1));
+cout<<res<<endl;
+return 0;
+}
