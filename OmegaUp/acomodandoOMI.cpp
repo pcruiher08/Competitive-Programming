f9302856dcbaa1183fcea20125d5b0d1c21de25b
unknown <pcruiher089@gmail.com>
2019-01-05 16:47:29 -0600

    omegaup

---
+++ OmegaUp/acomodandoOMI.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+int n;
+cin>>n;
+vi nums;
+int x;
+FOR(i,0,n,1){cin>>x; nums.pb(x);}
+int busca = nums[0];
+sort(nums.begin(), nums.end());
+FOR(i,0,n,1)if(nums[i]==busca){cout<<i<<endl; break;}
+return 0;
+}
