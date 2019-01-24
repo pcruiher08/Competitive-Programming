6384ca8f512763c3f6019cb6ddaec6c7e923155b
unknown <pcruiher089@gmail.com>
2019-01-24 16:04:28 -0600

    omegaup

--- OmegaUp/factorialSimple.cpp
+++ OmegaUp/factorialSimple.cpp
@@ -24,11 +24,22 @@
 #define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define vi vector<int>
 using namespace std;
-
+unsigned long long fact[100001];
+unsigned long long actual = 1;
+unsigned long long fac(long long n){
+    fact[0]=1;
+    fact[1]=1;
+    if(n>actual){
+    for(unsigned long long i=actual; i<=n; i++){
+        fact[i]=(fact[i-1]*i)%mod;
+    }
+    actual = n;
+    }
+    return fact[n];
+}
 int main(){
 sync;
-int n; cin>>n;
-string a; cin>>a;
-
+int m; cin>>m;
+cout<<fac(m)<<endl;
 return 0;
 }
