7a2da75782ed6491127d67ea228fd86e6c21c610
unknown <pcruiher089@gmail.com>
2019-01-31 10:21:43 -0600

    omegaup

--- OmegaUp/cadenasFormato.cpp
+++ OmegaUp/cadenasFormato.cpp
@@ -25,19 +25,34 @@
 #define vi vector<int>
 using namespace std;
 
+bool isVowel(char a){
+    return (a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U');
+}
+
 void minusc(string a){
-    FOR(i,0,a.length(),1)cout<<a[i].tolower();
+    FOR(i,0,a.length(),1)cout<<(char)tolower(a[i]);
     cout<<endl;
 }
 
 void mayus(string a){
-    FOR(i,0,a.length(),1)cout<<a[i].toupper();
+    FOR(i,0,a.length(),1)cout<<(char)toupper(a[i]);
     cout<<endl;
 }
 
+
 void titulo(string a){
-    cout<<a[0].tolower();
-    FOR(i,1,a.length(),1)cout<<a[i].tolower();
+    cout<<(char)tolower(a[0]);
+    FOR(i,1,a.length(),1)cout<<(char)tolower(a[i]);
+    cout<<endl;
+}
+
+void cap(string a){
+    FOR(i,0,a.length(),1)if(a[i]==' '&&i<a.length())cout<<" "<<(char)toupper(a[++i]); else cout<<(char)tolower(a[i]);
+    cout<<endl;
+}
+
+void change(string a){
+    FOR(i,0,a.length(),1)if(isupper(a[i])) cout<<(char)tolower(a[i]); else cout<<(char)toupper(a[i]);
     cout<<endl;
 }
 
@@ -48,7 +63,7 @@ string a; getline(cin,a);
 minusc(a);
 mayus(a);
 titulo(a);
-//cap(a);
-//vocal(a);
+cap(a);
+change(a);
 return 0;
 }
