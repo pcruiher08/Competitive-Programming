cf6218fc79e3de1c9f45577fb3178d75bfb300ca
unknown <pcruiher089@gmail.com>
2019-02-27 20:12:56 -0600

    two problems

---
+++ OmegaUp/closestPair.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+struct flight{
+    string name;
+    int x, y;
+    bool operator <(const flight& pt) const{
+        return (x < pt.x) || ((!(pt.x < x)) && (y < pt.y));
+    }
+};
+
+int main(){
+sync;
+int n; cin>>n;
+vector <flight> vuelos; 
+
+flight aux;
+while(cin>>aux.name>>aux.x>>aux.y){
+    vuelos.pb(aux);
+}
+sort(vuelos.begin, vuelos.end);
+cout<<vuelos[0].name<<" "<<vuelos[1].name;
+return 0;
+}
