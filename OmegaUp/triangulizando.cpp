ff694b535ff523f87173789a08ce42665e764a6b
unknown <pcruiher089@gmail.com>
2019-04-06 22:06:45 -0600

    new problems

--- OmegaUp/triangulizando.cpp
+++ OmegaUp/triangulizando.cpp
@@ -29,18 +29,17 @@ int main(){
 sync;
 int arr[3]; cin>>arr[0]>>arr[1]>>arr[2];
 
-if( (arr[0]==arr[1]) && (arr[1]==arr[2]) ){
-    cout<<"EQUILATERO"<<endl;
-    return 0;
-}
-sort(arr,arr+3);
-
-if(arr[1]+arr[0]<arr[2]){
+if(arr[1]+arr[0]<arr[2] || arr[0]+arr[2]<arr[1] || arr[1]+arr[2]<arr[0] || arr[0]==0 ||arr[1]==0 ||arr[2]==0){
     cout<<"NO PROFESOR"<<endl;  
     return 0;
 }
 
-if( (arr[0]==arr[1]) || (arr[1]==arr[2]) ){
+if( (arr[0]==arr[1]) && (arr[1]==arr[2]) ){
+    cout<<"EQUILATERO"<<endl;
+    return 0;
+}
+
+if( (arr[0]==arr[1]) || (arr[1]==arr[2]) || arr[0]==arr[2]){
     cout<<"ISOSCELES"<<endl;
     return 0;
 }
