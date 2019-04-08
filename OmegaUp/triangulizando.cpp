06cf69fb87c30540a9e4607b02e43539f02187ef
unknown <pcruiher089@gmail.com>
2019-04-07 20:19:01 -0500

    new

--- OmegaUp/triangulizando.cpp
+++ OmegaUp/triangulizando.cpp
@@ -27,7 +27,7 @@ using namespace std;
 
 int main(){
 sync;
-int arr[3]; cin>>arr[0]>>arr[1]>>arr[2];
+double arr[3]; cin>>arr[0]>>arr[1]>>arr[2];
 
 if(arr[1]+arr[0]<arr[2] || arr[0]+arr[2]<arr[1] || arr[1]+arr[2]<arr[0] || arr[0]==0 ||arr[1]==0 ||arr[2]==0){
     cout<<"NO PROFESOR"<<endl;  
