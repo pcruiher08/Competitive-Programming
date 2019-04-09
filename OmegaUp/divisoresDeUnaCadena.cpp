5f906d660a8fc283b408a6f3f5bde062ac46a054
unknown <pcruiher089@gmail.com>
2019-04-09 17:19:26 -0500

    new contest done, first place

---
+++ OmegaUp/divisoresDeUnaCadena.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+
+int countDivisors(int n) { 
+    int cnt = 0; 
+    for (int i = 1; i <= sqrt(n); i++) { 
+        if (n % i == 0) { 
+            if (n / i == i) 
+                cnt++; 
+            else
+                cnt = cnt + 2; 
+        } 
+    } 
+    return cnt; 
+} 
+
+int main(){
+sync;
+string a; cin>>a;
+int sum = 0;
+FOR(i,0,a.length(),1){sum+=toupper(a[i]);}
+cout<<sum<<endl;
+cout<<countDivisors(sum)<<endl;
+
+return 0;
+}
