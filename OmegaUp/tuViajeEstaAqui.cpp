3f35828f23f417b1ec2ba32e5dc9e21d46e509c0
unknown <pcruiher089@gmail.com>
2019-04-24 20:37:47 -0500

    update

--- OmegaUp/tuViajeEstaAqui.cpp
+++ OmegaUp/tuViajeEstaAqui.cpp
@@ -36,15 +36,11 @@ int main(){
 	cin >> s1 >> s2;
 	ull prod1 = 1;
 	for (int i=0;i<s1.size();i++){
-		prod1 *= (int) (s1[i]-'A'+1);//+1;
-		//cout<<(s1[i]-'A'+1)<<" ";
-		//cout<<s1[i]<<" ";
+		prod1 *= (int) (s1[i]-'A'+1);
 	}
-	//cout<<endl;
-	//cout<<prod1<<endl;
 	ull prod2 = 1;
 	for (int i=0;i<s2.size();i++){
-		prod2 *= (int) (s2[i]-'A'+1);//+1;
+		prod2 *= (int) (s2[i]-'A'+1);
 	}
   cout << (prod1%47==prod2%47?"GO":"STAY") << endl;
   }
