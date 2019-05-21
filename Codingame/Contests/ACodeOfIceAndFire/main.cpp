fd2e1e8ad90c7806fd1b485cca5e46a0d05df741
unknown <pcruiher089@gmail.com>
2019-05-21 10:27:56 -0700

    new code AI

--- Codingame/Contests/ACodeOfIceAndFire/main.cpp
+++ Codingame/Contests/ACodeOfIceAndFire/main.cpp
@@ -6,12 +6,7 @@
 #include <stack>
 #include <chrono>
 #define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-using namespace std;
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -69,30 +64,98 @@ Player::Player(int x, int y, int id){
 
 
 class Tile{
-    Tile();
-    Tile(int, int, char);
-    char type;
-    int x; 
-    int y;
+    public:
+        Tile();
+        Tile(int x, int y, char type);
+        char type;
+        int x; 
+        int y;
+        void setX(int y){this -> y = y;};
+        void setY(int x){this -> x = x;};
+        void setType(char type){this -> type = type;};
+        bool isVoid, isNeutral, isOwnedAndActive, isOwnedAndInactive, isOpponentAndActive, isOpponentAndInactive;
 };
 
 Tile::Tile(){
     this -> x = 0;
     this -> y = 0;
     this -> type = ' ';
+    isVoid = false;
+    isNeutral = false; 
+    isOwnedAndActive = false;
+    isOwnedAndInactive = false;
+    isOpponentAndActive = false; 
+    isOpponentAndInactive = false;
 }
 
 Tile::Tile(int x, int y, char type){
     this -> x = x;
     this -> y = y;
     this -> type = type;
+    isVoid = false;
+    isNeutral = false; 
+    isOwnedAndActive = false;
+    isOwnedAndInactive = false;
+    isOpponentAndActive = false; 
+    isOpponentAndInactive = false;
+    if(type = '#') isVoid = true;
+    if(type = '.') isNeutral = true;
+    if(type = 'O') isOwnedAndActive = true; 
+    if(type = 'o') isOwnedAndInactive = true; 
+    if(type = 'X') isOpponentAndActive = true; 
+    if(type = 'x') isOpponentAndInactive = true;
+}
+class Building{
+    public:
+        int x, y, owner,type;
+        Building();
+        Building(int,int,int,int);
+};
+
+Building::Building(){
+    x = 0; 
+    y = 0; 
+    owner = 0; 
+    type = 0;
 }
 
-Player player(); 
-Player enemy();
+Building::Building(int x, int y, int owner, int type){
+    this -> x = x; 
+    this -> y = y;
+    this -> owner = owner; 
+    this -> type = type;
+}
+
+
+class Unit{
+  public:
+    int owner; 
+    int unitID;
+    int level; 
+    int x, y;
+    Unit();
+    Unit(int,int,int,int);
+};
+
+Unit::Unit(){
+    x = 0; 
+    y = 0; 
+    unitID = 0; 
+    level = 0;
+}
+
+Unit::Unit(int x, int y, int unitID, int level){
+    this -> x = x; 
+    this -> y = y;
+    this -> unitID = unitID; 
+    this -> level = level;
+}
+
 Tile Map[12][12];
 int main(){
 
+Player *player = new Player(); 
+Player *enemy = new Player();
  
     int numberMineSpots;
     cin >> numberMineSpots;
@@ -112,33 +175,46 @@ int main(){
         cin >> gold;
         int income;
         cin >> income;
-        player.gold = gold; 
-        cout<<player.gold;
-        player.income = income;
+        player->gold = gold; 
+        player->income = income;
 
         int opponentGold;
         cin >> opponentGold;
         int opponentIncome;
         cin >> opponentIncome;
 
-        enemy.gold = opponentGold;
-        enemy.income = opponentIncome;
+        enemy->gold = opponentGold;
+        enemy->income = opponentIncome;
 
         for (int i = 0; i < 12; i++) {
             string line;
             cin >> line;
+            int j = 0;
+            for(int i=0; i<12; i++){
+                //cerr<<line<<endl;
+                Tile aux(i,j++,line[i]);
+                Map[i][j] = aux;
+                cerr<<Map[i][j].type;
+            }
+            cerr<<endl;
         }
+
         int buildingCount;
         cin >> buildingCount;
+        
+        vector<Building> buildings;
         for (int i = 0; i < buildingCount; i++){
             int owner;
             int buildingType;
             int x;
             int y;
             cin >> owner >> buildingType >> x >> y;
+            Building aux(x,y,owner, buildingType);
+            buildings.push_back(aux);
         }
         int unitCount;
         cin >> unitCount;
+        vector <Unit> units;
         for (int i = 0; i < unitCount; i++) {
             int owner;
             int unitId;
@@ -146,6 +222,8 @@ int main(){
             int x;
             int y;
             cin >> owner >> unitId >> level >> x >> y;
+            Unit aux(x,y,unitId, level);
+            units.push_back(aux);
         }
 
 
