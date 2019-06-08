a34baf5dabef2a5ebfd7348c70bdd4583ba2ee67
unknown <pcruiher089@gmail.com>
2019-06-08 16:51:27 -0700

    new AC

---
+++ COJ/4316.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795028841971
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+void fastscan(int &number)  // from geeks4geeks
+{ 
+    //variable to indicate sign of input number 
+    bool negative = false; 
+    register int c; 
+  
+    number = 0; 
+  
+    // extract current character from buffer 
+    c = getchar(); 
+    if (c=='-') 
+    { 
+        // number is negative 
+        negative = true; 
+  
+        // extract the next character from the buffer 
+        c = getchar(); 
+    } 
+  
+    // Keep on extracting characters if they are integers 
+    // i.e ASCII Value lies from '0'(48) to '9' (57) 
+    for (; (c>47 && c<58); c=getchar()) 
+        number = number *10 + c - 48; 
+  
+    // if scanned input has a negative sign, negate the 
+    // value of the input number 
+    if (negative) 
+        number *= -1; 
+}
+
+int main(){
+sync;
+int n, s;
+while(cin>>n>>s){
+    int arr[n];
+    map<int, int> m;
+    bool found = false;
+    memset(arr,0,sizeof(arr));
+    FOR(i,0,n,1){int aux; cin>>aux; if(i>0)arr[i] = (arr[i-1] + aux); else arr[i] = aux;}
+
+    FORb(i,0,n,1,!found){
+        if(arr[i]==s){
+            found = true;
+        }else{
+            m.insert(make_pair(arr[i]+s,i+1));
+        }
+        if(!found){ 
+            if(m.find(arr[i]) != m.end()){
+                found = true;
+                cout << (int)m.find(arr[i])->second + 1 <<" " << i+1<<endl;
+            }
+        }else{
+            cout<<1<<" "<<i+1<<endl;
+        }
+
+    }
+    if(!found)cout<<-1<<endl;
+
+}
+return 0;
+}
