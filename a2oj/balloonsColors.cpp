2d75e0639c65c2c33b85b9a8259990474536a731
unknown <pcruiher089@gmail.com>
2019-08-06 11:53:37 -0500

    3 AC from ladder

---
+++ a2oj/balloonsColors.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795028841971
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+int k; cin>>k; 
+while(k--){
+    int n, x, y; cin>>n>>x>>y;
+    int easiest, hardest; 
+    cin>>easiest; 
+    while((n--)-2) cin>>hardest;
+    cin>>hardest;
+    if(easiest == x && hardest == y){
+        cout<<"BOTH"<<endl;
+    }else if(easiest == x){
+        cout<<"EASY"<<endl;
+    }else if(hardest == y){
+        cout<<"HARD"<<endl;
+    }else{
+        cout<<"OKAY"<<endl;
+    }
+}
+
+
+return 0;
+}
