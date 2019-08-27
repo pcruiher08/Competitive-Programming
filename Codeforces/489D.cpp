244894eac902ca77dbd990b7e37b4029b63cb3ce
unknown <pcruiher089@gmail.com>
2019-08-27 14:52:57 -0500

    AC

--- Codeforces/489D.cpp
+++ Codeforces/489D.cpp
@@ -33,7 +33,7 @@ int nodos; cin>>nodos;
 int edges; cin>>edges;
 
 int matAdj[nodos][nodos];
-//memset(matAdj,0,sizeof(matAdj));
+
 FOR(i,0,nodos,1)FOR(j,0,nodos,1)matAdj[i][j]=0;
 
 FOR(i,0,edges,1){
@@ -44,7 +44,6 @@ FOR(i,0,edges,1){
 
 int matrizDeNodos[nodos][nodos];
 
-//memset(matrizDeNodos,0,sizeof(matrizDeNodos));
 FOR(i,0,nodos,1)FOR(j,0,nodos,1)matrizDeNodos[i][j]=0;
 
 int piso = 0; 
@@ -52,26 +51,15 @@ int acumulador = 0;
 
 while(piso<nodos){
     FOR(i,0,nodos,1){
-        //cout<<matAdj[piso][i]<<" ";
         if(matAdj[piso][i]){
             FOR(j,0,nodos,1)
             if(matAdj[i][j])
             matrizDeNodos[piso][j]++;
         }
     }
-    //cout<<endl;
     piso++;
 }
-/*
-cout<<"----------------"<<endl;
 
-for(int i= 0; i<nodos; i++){
-    for(int j= 0; j<nodos; j++){
-        cout<<matrizDeNodos[i][j]<<" ";
-    }
-    cout<<endl;
-}
-*/
 FOR(i,1,nodos+1,1)FOR(j,1,nodos+1,1)if(i!=j)acumulador+=gauss(matrizDeNodos[i-1][j-1]-1);
 
 cout<<acumulador<<endl;
