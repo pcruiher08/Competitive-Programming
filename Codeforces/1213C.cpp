bba159c07fa15393e8be63a7fd8f481a641a9270
unknown <pcruiher089@gmail.com>
2019-08-30 20:00:25 -0500

    update

---
+++ Codeforces/1213C.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795028841971
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+using namespace std;
+
+int main(){
+sync;
+int n; cin>>n;
+while(n--){
+    ull a, b; 
+    cin>>a>>b;
+    ull sum =0;
+    ull num = 0;
+    FOR(i,1,a/b+1,1){
+        num += b;
+        sum += num % 10;
+    }
+    cout<<sum<<endl;
+}
+
+return 0;
+}
--- UVa/11283-PlayingBoogle.cpp
+++ UVa/11283-PlayingBoogle.cpp
@@ -46,6 +46,7 @@ FOR(o,0,n,1){
     int queries; cin>>queries; 
     int points = 0;
     while(queries--){
+        //points = 0;
         string query; cin>>query;
         int queryLetters[26]; memset(queryLetters,0,sizeof(queryLetters));
         FOR(i,0,query.length(),1) queryLetters[query[i]-'A']++;
@@ -62,13 +63,25 @@ FOR(o,0,n,1){
                     break;
                 }
             }
-            if(flag){
-                if(len==4||len == 3)points+=1;
-                if(len==5)points+=2;
-                if(len==6)points+=3; 
-                if(len==7)points+=5;
-                if(len>=8)points+=11;
-            }
+        }
+        if(flag){
+            /*
+            3 - > 1 
+            4 - > 1 
+            5 - > 2
+            6 - > 3
+            7 - > 5
+            8 - > 11
+            */
+            cout<<"found : "<<query<<endl;
+            int toSum = 0;
+            if(len==4||len == 3)toSum=1;
+            if(len==5)toSum=2;
+            if(len==6)toSum=3; 
+            if(len==7)toSum=5;
+            if(len>=8)toSum=11;
+            points += toSum;
+            cout<<"+ "<< toSum<<endl;
         }
     }
 
