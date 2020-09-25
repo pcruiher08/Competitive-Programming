06a0b232a5debf7a9ade0d6bef00eb9bc0d5d12e
Pablo C. Ruíz <pcruiher089@gmail.com>
2020-09-24 19:59:24 -0500

    more

---
+++ Codeforces/227B.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <map>
+#include <list>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <climits>
+#define ll long long
+#define ull unsigned long long
+#define FOR(m,s,n,u) for(int m=s; m<n; m+=u)
+#define ROF(m,s,n,u) for(int m=n; m>=s; m-=u)
+#define FORb(m,s,n,u,k) for(int m=s; m<n&&k; m+=u)
+#define pb push_back
+#define mp make_pair
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795028841971
+#define INF 0x3f3f3f3f
+#define EPS 1e-9
+#define MOD(x) ((x%mod)+mod)%mod
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define vi vector<int>
+#define pii pair<int,int>
+using namespace std;
+
+int main(){
+sync;
+ll n; cin>>n;
+map<ll,ll> m;
+FOR(i,0,n,1){
+    ll x; cin>>x;
+    m[x] = i;
+}
+ll k; cin>>k;
+ll vas = 0, pet = 0;
+while(k--){
+    ll lec; cin>>lec;
+    vas+=m[lec]+1;
+    pet+=n-m[lec];
+}
+cout<<vas<<" "<<pet;
+return 0;
+}
