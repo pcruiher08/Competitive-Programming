c48d5cf1deef1d6e322311b2f23b61e4653e4f10
Pablo C. Ruíz <pcruiher089@gmail.com>
2020-10-01 12:38:18 -0500

    mathvault

--- MathVault/problem1.py
+++
@@ -1,5 +0,0 @@
-count = 0
-for i in range(67,997+1,15):
-    count+=1
-    print(i)
-print(count)
--- MathVault/problem2.py
+++
@@ -1,4 +0,0 @@
-probabilidadBien = 1/5
-probabilidadMal = 1 - probabilidadBien
-
-print(probabilidadBien**4 + probabilidadMal**16)
