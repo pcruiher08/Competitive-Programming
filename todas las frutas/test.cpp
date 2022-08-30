363873d6a26910f78674366644987ac94689edd8
Pablo C. Ruíz <pcruiher089@gmail.com>
2022-08-30 12:10:00 -0700

    cleanup

--- todas las frutas/test.cpp
+++ todas las frutas/test.cpp
@@ -1,30 +1,14 @@
-Process Ping{
-    P(semaforoPapas);
-    Ping();
-    V(semaforoPing);
+
+#include <IBCEGS.h>
+
+string classificationFunction(){
+    if(piano == true){
+        return "??????";
+    }
 }
 
-Process Pong{
-    P(semaforoPing);
-    Pong();
-    V(semaforoPong);
+using namespace std;
+
+int main(){
+    cout<<classificationFunction()<<endl;
 }
-
-Process Papas{
-    P(semaforoPong);
-    Papas();
-    V(semaforoPapas);
-}
-
-main(){
-    initSem(semaforoPong,0);
-    initSem(semaforoPing,0);
-    initSem(semaforoPapas,1);
-    parBegin; //concurrente 
-        Pong;
-        Papas;
-        Ping;
-    parEnd;
-}
-
-
